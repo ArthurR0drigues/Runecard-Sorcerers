6382acd649fd7201217f96bef72f576a090f28a0
ArthurR0drigues <108202997+ArthurR0drigues@users.noreply.github.com>
2022-10-24 22:36:45 -0300

    Update batalha.js

--- batalha.js
+++ batalha.js
@@ -6,8 +6,17 @@ let minion = {
     vidaMaxima: 2, 
     dano: 1, 
     custo: 1,
-    imagem: 'img/Runa-Logo.png',
+    imagem: 'img/carta.png',
     id: 0 
+}; 
+
+let cavaleiro = {
+    vida: 4, 
+    vidaMaxima: 4, 
+    dano: 2, 
+    custo: 2,
+    imagem: 'img/background2.jpg',
+    id: 1 
 }; 
 
 /*fim das cartas */
@@ -15,12 +24,36 @@ let minion = {
 function adicionarAoBaralho(carta){
     baralho[baralho.length] = carta; 
 }; 
+
 function criarBaralho(baralho) {
     for (let i=0; i < baralho.length; i++){
-        console.log("aBA"); 
         let cartaEl = document.createElement("img"); 
         cartaEl.src = baralho[i].imagem; 
+        cartaEl.classList.add('drag');
         let areaEl = document.querySelector('#baralho-area');
         areaEl.appendChild(cartaEl); 
     }
 }; 
+/*
+function embaralhar(baralho) {
+for (let i = 0; i < baralho.length; i--) {
+    baralho[getRandomInt(baralho.length)] = baralho[getRandomInt(baralho.length)];
+}
+return baralho;
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+
+let bodyEl = document.querySelector('body');
+let CartaTopoEl = document.querySelectorAll('.drag'); 
+
+function selecionarCarta(e){
+    CartaTopoEl[0].style.left = `${e.pageX}px`;
+    CartaTopoEl[0].style.top = `${e.pageY}px`;
+}
+
+bodyEl.addEventListener('mousemove', selecionarCarta);
+*/
