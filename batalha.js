1022a943a2d31507b47d085935fc39be5ef6f6e7
ArthurR0drigues <108202997+ArthurR0drigues@users.noreply.github.com>
2022-10-26 22:28:53 -0300

    Update batalha.js

--- batalha.js
+++ batalha.js
@@ -1,59 +1,96 @@
-/*vetor com as carts do jogador*/
-let baralho = []; 
+/*vetor com as cartas do jogador*/
+let baralho = [];
+/*vetor com as cartas na mao do jogador*/
+let deck = [];
 /*todas as cartas */
 let minion = {
-    vida: 2, 
-    vidaMaxima: 2, 
-    dano: 1, 
+    vida: 2,
+    vidaMaxima: 2,
+    dano: 1,
     custo: 1,
     imagem: 'img/carta.png',
-    id: 0 
-}; 
+    id: 0
+};
 
 let cavaleiro = {
-    vida: 4, 
-    vidaMaxima: 4, 
-    dano: 2, 
+    vida: 4,
+    vidaMaxima: 4,
+    dano: 2,
     custo: 2,
     imagem: 'img/background2.jpg',
-    id: 1 
-}; 
+    id: 1
+};
 
 /*fim das cartas */
 
-function adicionarAoBaralho(carta){
-    baralho[baralho.length] = carta; 
-}; 
-
 function criarBaralho(baralho) {
-    for (let i=0; i < baralho.length; i++){
-        let cartaEl = document.createElement("img"); 
-        cartaEl.src = baralho[i].imagem; 
+    for (let i = 0; i < baralho.length; i++) {
+        let cartaEl = document.createElement("img");
+        cartaEl.src = baralho[i].imagem;
         cartaEl.classList.add('drag');
         let areaEl = document.querySelector('#baralho-area');
-        areaEl.appendChild(cartaEl); 
+        areaEl.appendChild(cartaEl);
     }
-}; 
-/*
+};
+
 function embaralhar(baralho) {
-for (let i = 0; i < baralho.length; i--) {
-    baralho[getRandomInt(baralho.length)] = baralho[getRandomInt(baralho.length)];
-}
-return baralho;
+    for (let i = 0; i < 5; i--) {
+        deck[i] = baralho[getRandomInt(baralho.length)];
+    }
+    return deck;
 }
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-
+/*
 let bodyEl = document.querySelector('body');
-let CartaTopoEl = document.querySelectorAll('.drag'); 
+let CartaTopoEl = document.querySelectorAll('.drag');
 
-function selecionarCarta(e){
+function selecionarCarta(e) {
     CartaTopoEl[0].style.left = `${e.pageX}px`;
     CartaTopoEl[0].style.top = `${e.pageY}px`;
 }
 
 bodyEl.addEventListener('mousemove', selecionarCarta);
 */
+let tutorial = {
+    oponente: 'El picachu',
+    img: 'img/carta-base.png',
+};  
+
+let boi = {
+    oponente: 'Dona Marta',
+    img: 'img/carta.png',
+};  
+
+let batalhasDisponiveis = [tutorial, boi];
+
+let oponenteAtual = -1;
+
+let anteriorEl = document.querySelector('#anterior');
+let proximoEl = document.querySelector('#proximo');
+let oponenteEl = document.querySelector('#oponente');
+let nomeOponenteEl = document.querySelector('#nome-oponente');
+
+anteriorEl.addEventListener('click', function () {
+    oponenteAtual--;
+    if (oponenteAtual < 0) {
+        oponenteAtual = batalhasDisponiveis.length-1;
+    }
+    oponenteEl.src = batalhasDisponiveis[oponenteAtual].img;
+    nomeOponenteEl.innerHTML = batalhasDisponiveis[oponenteAtual].oponente;
+});
+
+proximoEl.addEventListener('click', proximo);
+function proximo()
+{
+    oponenteAtual++;
+    if (oponenteAtual > batalhasDisponiveis.length-1) {
+        oponenteAtual = 0;
+    }
+    oponenteEl.src = batalhasDisponiveis[oponenteAtual].img;
+    nomeOponenteEl.innerHTML = batalhasDisponiveis[oponenteAtual].oponente;
+};
+proximo();
