b65bb641c9b8baf81de066cabcbeeb7c65303a09
ArthurR0drigues <108202997+ArthurR0drigues@users.noreply.github.com>
2022-10-31 23:49:19 -0300

    Update batalha.js

--- batalha.js
+++ batalha.js
@@ -1,14 +1,10 @@
-/*vetor com as cartas do jogador*/
-let baralho = [];
-/*vetor com as cartas na mao do jogador*/
-let deck = [];
 /*todas as cartas */
 let minion = {
     vida: 2,
     vidaMaxima: 2,
     dano: 1,
     custo: 1,
-    imagem: 'img/carta.png',
+    imagem: 'img/minion.png',
     id: 0
 };
 
@@ -17,23 +13,51 @@ let cavaleiro = {
     vidaMaxima: 4,
     dano: 2,
     custo: 2,
-    imagem: 'img/background2.jpg',
+    imagem: 'img/knight.png',
     id: 1
 };
 
 /*fim das cartas */
 
-/*
-function criarBaralho(baralho) {
-    for (let i = 0; i < baralho.length; i++) {
+/* vetor com todas as cartas desbloqueadas pelo jogador */
+let allCard = [minion, cavaleiro, minion];
+/*vetor com as cartas do jogador*/
+let baralho = [];
+/*vetor com as cartas na mao do jogador*/
+let deck = [];
+
+
+let colecaoEl = document.querySelector('#colecao');
+let baralhoEl = document.querySelector('#baralho-formado');
+function criarAllcard(vetortipo, paiEl) {
+    for (let i = 0; i < vetortipo.length; i++) {
         let cartaEl = document.createElement("img");
-        cartaEl.src = baralho[i].imagem;
-        cartaEl.classList.add('drag');
-        let areaEl = document.querySelector('#baralho-area');
-        areaEl.appendChild(cartaEl);
+        cartaEl.src = vetortipo[i].imagem;
+        cartaEl.addEventListener('click', function () {
+            if (baralho.length == 10 || cartaEl.classList.contains('adicionar-baralho'))
+                return console.log("limite");
+            baralho.push(vetortipo[i]);
+            baralho.slice(0, baralho.length);
+            baralhoEl.innerText = "";
+            criarBaralho(baralho, baralhoEl);
+            cartaEl.classList.toggle('adicionar-baralho');
+        });
+        paiEl.appendChild(cartaEl);
     }
 };
-
+function criarBaralho(vetortipo, paiEl) {
+    for (let i = 0; i < vetortipo.length; i++) {
+        let cartaEl = document.createElement("img");
+        cartaEl.src = vetortipo[i].imagem;
+        cartaEl.addEventListener('click', function () {
+            vetortipo.slice(cartaEl);
+        });
+        paiEl.appendChild(cartaEl);
+    }
+};
+criarAllcard(allCard, colecaoEl);
+criarBaralho(baralho, baralhoEl);
+/*
 function embaralhar(baralho) {
     for (let i = 0; i < 5; i--) {
         deck[i] = baralho[getRandomInt(baralho.length)];
@@ -58,24 +82,24 @@ bodyEl.addEventListener('mousemove', selecionarCarta);
 */
 
 let oponenteEl = document.querySelector('#oponente');
-let objectImageEl = document.querySelector('#oponente-obj'); 
-function capturaOponenteImagem(){
-    objectImageEl.style.backgroundImage = 'url(' + oponenteEl.src + ')'; 
+let objectImageEl = document.querySelector('#oponente-obj');
+function capturaOponenteImagem() {
+    objectImageEl.style.backgroundImage = 'url(' + oponenteEl.src + ')';
 }
-let botaoOponenteEl = document.querySelector('#botao-oponente'); 
-botaoOponenteEl.addEventListener('click', capturaOponenteImagem); 
+let botaoOponenteEl = document.querySelector('#botao-oponente');
+botaoOponenteEl.addEventListener('click', capturaOponenteImagem);
 
 
 /* JOGAR */
 let tutorial = {
     oponente: 'El picachu',
     img: 'img/carta-base.png',
-};  
+};
 
 let boi = {
     oponente: 'Dona Marta',
     img: 'img/carta.png',
-};  
+};
 
 let batalhasDisponiveis = [tutorial, boi];
 
@@ -89,17 +113,16 @@ let nomeOponenteEl = document.querySelector('#nome-oponente');
 anteriorEl.addEventListener('click', function () {
     oponenteAtual--;
     if (oponenteAtual < 0) {
-        oponenteAtual = batalhasDisponiveis.length-1;
+        oponenteAtual = batalhasDisponiveis.length - 1;
     }
     oponenteEl.src = batalhasDisponiveis[oponenteAtual].img;
     nomeOponenteEl.innerHTML = batalhasDisponiveis[oponenteAtual].oponente;
 });
 
 proximoEl.addEventListener('click', proximo);
-function proximo()
-{
+function proximo() {
     oponenteAtual++;
-    if (oponenteAtual > batalhasDisponiveis.length-1) {
+    if (oponenteAtual > batalhasDisponiveis.length - 1) {
         oponenteAtual = 0;
     }
     oponenteEl.src = batalhasDisponiveis[oponenteAtual].img;
