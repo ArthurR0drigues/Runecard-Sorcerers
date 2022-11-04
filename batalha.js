881fdf024371ba37efd0ac6ce4a0848939772983
ArthurR0drigues <108202997+ArthurR0drigues@users.noreply.github.com>
2022-11-03 22:12:53 -0300

    Update batalha.js

--- batalha.js
+++ batalha.js
@@ -1,31 +1,3 @@
-/*todas as cartas */
-let minion = {
-    vida: 2,
-    vidaMaxima: 2,
-    dano: 1,
-    custo: 1,
-    imagem: 'img/minion.png',
-    id: 0
-};
-
-let cavaleiro = {
-    vida: 4,
-    vidaMaxima: 4,
-    dano: 2,
-    custo: 2,
-    imagem: 'img/knight.png',
-    id: 1
-};
-
-/*fim das cartas */
-
-/* vetor com todas as cartas desbloqueadas pelo jogador */
-let allCard = [minion, cavaleiro, minion];
-/*vetor com as cartas do jogador*/
-let baralho = [];
-/*vetor com as cartas na mao do jogador*/
-let deck = [];
-
 /*
 function embaralhar(baralho) {
     for (let i = 0; i < 5; i--) {
@@ -50,40 +22,8 @@ function selecionarCarta(e) {
 bodyEl.addEventListener('mousemove', selecionarCarta);
 */
 
-//criar vetores
-let colecaoEl = document.querySelector('#colecao');
-let baralhoEl = document.querySelector('#baralho-formado');
-function criarAllcard(vetortipo, paiEl) {
-    for (let i = 0; i < vetortipo.length; i++) {
-        let cartaEl = document.createElement("img");
-        cartaEl.src = vetortipo[i].imagem;
-        cartaEl.addEventListener('click', function () {
-            if (baralho.length == 10 || cartaEl.classList.contains('adicionar-baralho'))
-                return console.log("limite");
-                baralho.push(vetortipo[i]);
-                baralho.slice(0, baralho.length);
-            baralhoEl.innerText = "";
-            criarBaralho(baralho, baralhoEl);
-            cartaEl.classList.toggle('adicionar-baralho');
-        });
-        if (paiEl == null){
-            return false; 
-        }
-        paiEl.appendChild(cartaEl);
-    }
-};
-function criarBaralho(vetortipo, paiEl) {
-    for (let i = 0; i < vetortipo.length; i++) {
-        let cartaEl = document.createElement("img");
-        cartaEl.src = vetortipo[i].imagem;
-        cartaEl.addEventListener('click', function () {
-            vetortipo.slice(cartaEl);
-        });
-        paiEl.appendChild(cartaEl);
-    }
-};
-criarAllcard(allCard, colecaoEl);
-criarBaralho(baralho, baralhoEl);
 
+/*vetor com as cartas na mao do jogador*/
+let deck = [];
 let objectImageEl = document.querySelector('#oponente-obj');
 objectImageEl.src = localStorage.getItem('oponente');
