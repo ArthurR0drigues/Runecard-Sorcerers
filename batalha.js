7cecdcdeaff94b9032e3ca0bf9d1e109a4293505
ArthurR0drigues <108202997+ArthurR0drigues@users.noreply.github.com>
2022-11-07 23:50:21 -0300

    Update batalha.js

--- batalha.js
+++ batalha.js
@@ -1,38 +1,33 @@
-/*
-function embaralhar(baralho) {
-    for (let i = 0; i < 5; i--) {
-        deck[i] = baralho[getRandomInt(baralho.length)];
-    }
-    return deck;
-}
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
-
-
-let bodyEl = document.querySelector('body');
-let CartaTopoEl = document.querySelectorAll('.drag');
-
-function selecionarCarta(e) {
-    CartaTopoEl[0].style.left = `${e.pageX}px`;
-    CartaTopoEl[0].style.top = `${e.pageY}px`;
-}
-
-bodyEl.addEventListener('mousemove', selecionarCarta);
-*/
-
-
 /*vetor com as cartas na mao do jogador*/
 let deck = [];
 let objectImageEl = document.querySelector('#oponente-obj');
 objectImageEl.src = localStorage.getItem('oponente');
 
 let baralhoJs = localStorage.getItem('baralho');
-deck = JSON.parse(baralhoJs);
+let baralhoObj = JSON.parse(baralhoJs);
 
 let playerdeckEl = document.querySelector('#player-deck');
 
+function AdicionarAoDeck (deck, baralhoJs){
+    for (let i = 0; i < 5; i++){
+        deck.push(baralhoJs[i]); 
+    }
+};
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+function embaralhar(baralho) {
+    let novoBaralho = []; 
+    let tam = baralho.length; 
+    for (let i = 0; i < tam; i++) {
+        let indice = getRandomInt(tam-1); 
+        novoBaralho[i] = baralho[indice];
+        baralho[indice] = 'apague'; 
+        baralho = baralho.filter(word => word != 'apague');
+    }
+    console.log(novoBaralho); 
+    return novoBaralho;
+}
 function criarDeck(deck, paiEl) {
     for (let i = 0; i < deck.length; i++) {
         let cartaEl = document.createElement("img");
@@ -43,5 +38,20 @@ function criarDeck(deck, paiEl) {
         });
         paiEl.appendChild(cartaEl);
     }
-};
+}
+
+baralhoObj = embaralhar(baralhoObj); 
+AdicionarAoDeck(deck, baralhoObj); 
 criarDeck(deck, playerdeckEl); 
+
+/*
+let bodyEl = document.querySelector('body');
+let CartaTopoEl = document.querySelectorAll('.drag');
+
+function selecionarCarta(e) {
+    CartaTopoEl[0].style.left = `${e.pageX}px`;
+    CartaTopoEl[0].style.top = `${e.pageY}px`;
+}
+
+bodyEl.addEventListener('mousemove', selecionarCarta);
+*/
